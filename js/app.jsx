5c43eb2b0d1c2bf148475a818e2afadaf722301a
Mammatos <mariannawaranska@gmail.com>
2017-05-16 11:27:16 +0200

    add buttons to menu

--- js/app.jsx
+++ js/app.jsx
@@ -8,6 +8,35 @@ IndexRoute,
 hashHistory
 } from 'react-router';
 
+class Button extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text: this.props.text
+        }
+    }
+    render() {
+        return <button type="button">{this.state.text}</button>
+    }
+}
+
+class Menu extends React.Component {
+    render() {
+        return <div>
+            <h1>Menu</h1>
+            <Button text="TOP 10"/>
+            <Button text="TOP 100"/>
+        </div>
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <div>
+            <Menu/>
+        </div>
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function(){
     
